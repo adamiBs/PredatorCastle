3606d911af26f4d11a038909479271d72c9c122e
Adam Ben Shmuel <bsadambs@gmail.com>
2017-06-28 14:16:56 +0300

    Server static files and homepage

--- PredatorClient/index.js
+++ PredatorClient/index.js
@@ -1,14 +1,17 @@
-const express = require('express')
-const app = express()
-const PORT = 3000
+const express = require('express');
+var path = require("path");
+const app = express();
+const PORT = 3000;
 
-app.get('/', function(req,res){
-       
-     //res.sendFile('index.html');
-    res.send('Hello World!');
+app.use('/static', express.static('public'))
+
+app.get('/', function (req, res) {
+
+    res.sendFile(path.join(__dirname + '/views/index.html'));
+    //res.send('Hello World!');
 
 });
 
 app.listen(PORT, function () {
-  console.log('Example app listening on port 3000!')
+    console.log('Example app listening on port 3000!')
 })
