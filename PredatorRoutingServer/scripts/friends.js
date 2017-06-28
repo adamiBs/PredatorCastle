27740d019acbe084c0391c2376253a81ebb66906
Adam Ben Shmuel <bsadambs@gmail.com>
2017-06-28 17:31:59 +0300

    Implement get all friends request for routing server.

--- PredatorRoutingServer/scripts/friends.js
+++ PredatorRoutingServer/scripts/friends.js
@@ -1,8 +1,20 @@
+var http = require('http');
+var seleniumServerDns = '127.0.0.1:8080'
+
 module.exports = {
-  foo: function () {
-    // whatever
-  },
-  bar: function () {
-    // whatever
-  }
+    getAllFriendsById: function (id, sucess) {
+        var options = {
+            uri: 'http://' + seleniumServerDns + '/friends/' + id,
+            headers: {
+                'User-Agent': 'Request-Promise'
+            },
+            json: true // Automatically parses the JSON string in the response
+        };
+
+        rp(options)
+            .then(sucess)
+            .catch(function (err) {
+                console.log('Error');
+            });
+    }
 };
