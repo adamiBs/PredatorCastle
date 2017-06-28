fff55bfd357022d9b8cd6009c40e6b211170482c
OmerUnger <omerunger7@gmail.com>
2017-06-28 14:53:08 +0300

    changed the load profile page

--- PredatorClient/public/scripts/js.js
+++ PredatorClient/public/scripts/js.js
@@ -3,3 +3,27 @@ $('#myModal').on('shown.bs.modal', function () {
 })
 
 $('.collapse').collapse()
+
+function userIDTypeChanged(){
+  if(document.getElementById("UIDRadio").checked){
+    document.getElementById("UIDInput").disabled = false;
+    document.getElementById("UIDBtn").disabled = false;
+    document.getElementById("URLInput").disabled = true;
+    document.getElementById("URLBtn").disabled = true;
+    document.getElementById("UIDInput").focus();
+  } else if(document.getElementById("URLRadio").checked){
+    document.getElementById("UIDInput").disabled = true;
+    document.getElementById("UIDBtn").disabled = true;
+    document.getElementById("URLInput").disabled = false;
+    document.getElementById("URLBtn").disabled = false;
+    document.getElementById("URLInput").focus();
+  }
+}
+
+function UIDLoad(){
+  
+}
+
+function URLLoad(){
+
+}
