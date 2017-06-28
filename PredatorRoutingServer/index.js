11f7065b4aeefb10497655f64828b1654af2564b
Adam Ben Shmuel <bsadambs@gmail.com>
2017-06-28 22:59:51 +0300

    fix idos bad code

--- PredatorRoutingServer/index.js
+++ PredatorRoutingServer/index.js
@@ -5,19 +5,6 @@ const father = require('./scripts/father');
 const app = express();
 const PORT = 2424;
 
-//app.use(function (req, res, next) {
-//    //    res.header("Access-Control-Allow-Origin", "192.168.1.24");
-//    res.header("Access-Control-Allow-Origin", "*");
-//    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-//    next();
-//});
-
-//app.all('/*', function (req, res, next) {
-//    res.header("Access-Control-Allow-Origin", "*");
-//    res.header("Access-Control-Allow-Headers", "X-Requested-With");
-//    next();
-//});
-
 app.use(cors());
 app.use(bodyParser.json())
 
