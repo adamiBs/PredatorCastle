59c55feea3123eeac9ff60adc5c1001b7e93c943
ozomdi <ozz14700@gmail.com>
2017-06-28 22:06:23 +0300

    get posts

--- PredatorSeleniumServer/hack.py
+++ PredatorSeleniumServer/hack.py
@@ -23,15 +23,23 @@ def login(username,usr_password):
 
 #~~~~~~~~~~~~~ User Methods
 def getFriends(usr_id):
-    d.get("https://www.facebook.com/search/" + usr_id + "/friends")
-    scrollDown()
+    d.get("https://m.facebook.com/search/" + usr_id + "/friends")
     strr = d.page_source
     return extractIdsFromHtml(d.page_source)
     #lst_ids = [strr[m.start()+15:m.start()+30] for m in list(re.finditer("profile.php",strr))]
     #lst_ids = [idd[0:re.search("[0-9]+",idd).end()] for idd in lst_ids]
     #return lst_ids
+
+def getUserPosts(usr_id):
+  d.get("https://m.facebook.com/" + usr_id )
+  scrollDown()
+  posts = d.find_elements_by_tag_name("article")
+  posts_by_user = [post for post in posts if re.search("profile.php.*id=" + usr_id,post.get_attribute('innerHTML'))]
+  return len(posts_by_user)
+
 def getUserGroups(usr_id):
-  
+  return 1
+
 def getMutualFriendsCount(usr_id1, usr_id2):
     d.get("https://www.facebook.com/browse/mutual_friends/?uid=" + usr_id1 + "&node=" + usr_id2)
     scrollDown()
@@ -73,7 +81,7 @@ def scrollDown():
 def extractIdsFromHtml(html):
     strr = html
     lst_ids = [strr[m.start() + 15:m.start() + 30] for m in list(re.finditer("profile.php", strr))]
-    lst_ids = [idd[0:re.search("[0-9]+", idd).end()] for idd in lst_ids]
+    lst_ids = [idd[0:re.search("id=[0-9]+", idd).end()] for idd in lst_ids]
     return lst_ids
 
 def ConvertDataToFile(data):
